683c5d87738e973f953dc93e24778f2248f10e89
HellNiore <ritweek@hotmail.com>
2016-05-12 01:18:00 +0530

    First Check in

--- PolymorphismAndInheritance/Program.cs
+++ PolymorphismAndInheritance/Program.cs
@@ -10,6 +10,46 @@ namespace PolymorphismAndInheritance
     {
         static void Main(string[] args)
         {
+
+            ClassB y = new ClassB();
+            ClassA x = new ClassA();
+            ClassB z = new ClassA();
+            y.AAA(); y.BBB(); y.CCC();
+            x.AAA(); x.BBB(); x.CCC();
+            z.AAA(); z.BBB(); z.CCC();
+            Console.ReadKey();
+
+        }
+    }
+    public class ClassB
+    {
+        public void AAA()
+        {
+            Console.WriteLine("ClassB AAA");
+        }
+        public void BBB()
+        {
+            Console.WriteLine("ClassB BBB");
+        }
+        public void CCC()
+        {
+            Console.WriteLine("ClassB CCC");
+        }
+    }
+
+    public class ClassA : ClassB
+    {
+        public void AAA()
+        {
+            Console.WriteLine("ClassA AAA");
+        }
+        public void BBB()
+        {
+            Console.WriteLine("ClassA BBB");
+        }
+        public void CCC()
+        {
+            Console.WriteLine("ClassA CCC");
         }
     }
 }
